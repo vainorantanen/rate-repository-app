f85dbe4e0361e70de75077c0e9c602d626b53fdb
Väinö Rantanen <vaino01@hotmail.fi>
2023-05-25 17:54:34 +0300

    tehty osan c alustukset

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,16 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
 
 import Main from './src/components/Main';
 
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
+
 const App = () => {
   return (
-    <>
-      <NativeRouter>
+    <NativeRouter>
+      <ApolloProvider client={apolloClient}>
         <Main />
-      </NativeRouter>
-      <StatusBar style="auto" />
-    </>
+      </ApolloProvider>
+    </NativeRouter>
   );
 };
 
